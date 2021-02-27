b199430c34c356bf6d7f444335ca96410e4a8814
BiniTutor <70373013+binitutor@users.noreply.github.com>
2021-02-27 00:33:51 -0500

    seventh commit, db connection, create booking

---
+++ createCal.js
@@ -0,0 +1,47 @@
+// on click, create booking hours on date for calendar 
+$("#saveRecord").on('click', function(e){
+    e.preventDefault();
+	// get values from form: date and time
+	var crDate    = $("#crDate").val();       
+    var crTime    = $("#crTime").val();
+    var available = "available";
+    
+    $.post("createCal.php",
+    {
+        crDate: crDate,
+        crTime: crTime,
+        available: available
+    });              
+    alert("Data saved Successfully!");
+    
+    var str = '<h3>Date: <span class="text-primary">' + crDate + '</span> <br>Time: <span class="text-primary">' + crTime + '</span></h3>';
+    $('#createdHours').html(str);
+});
+
+
+
+$('#fetchHours').click(function(e){
+    e.preventDefault();
+    // get data from hourCalendar and display on modal
+   	$.get("fetchHours.php", function(data, status){
+        var obj = jQuery.parseJSON( data );
+		var str = '<tr><th>#</th><th>Date</th><th>Time</th><th>Availability</th></tr>'; 
+							
+		$.each(obj, function(key,value) {
+			str += '<tr><td>' + value.id + '</td>';
+			str += '<td>' + value.crDate + '</td>';
+			str += '<td>' + value.crTime + '</td>';
+			str += '<td>' + value.available + '</td></tr>';
+        });
+		
+		str += '';	      
+		
+		// add data to modal tbody
+   		$('#createdHoursDb').html(str);
+   					        
+    }); // ... get method ends
+}); 
+
+
+
+
