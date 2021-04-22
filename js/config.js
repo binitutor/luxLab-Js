57ad9757ab18d08ce9f1bb0a99467531cf1c44d4
BiniTutor <70373013+binitutor@users.noreply.github.com>
2021-04-22 17:07:30 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_0BpnKLcYo', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+        clientId: '6sfksp38pq48t3meo00912mtsf' //is this used anywhere?
+    },
+};
